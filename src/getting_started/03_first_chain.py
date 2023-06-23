a171c2b083421e04dc0b258b372179dde82036a2
Sachin Kulkarni <sachin.kulkarni.iimc@gmail.com>
2023-06-23 14:31:07 +0200

    Update 03_first_chain.py

--- src/getting_started/03_first_chain.py
+++ src/getting_started/03_first_chain.py
@@ -5,7 +5,7 @@ from typing import Dict
 
 
 prompt_template = "What is a good name for a company that makes {product}?"
-endpoint_name = "vicuna-13b-1-1-2023-06-03-15-48-50-242"
+endpoint_name = "flan-t5-xxl-2023-06-05-13-34-25-298"
 
 
 class ContentHandler(LLMContentHandler):
@@ -15,23 +15,24 @@ class ContentHandler(LLMContentHandler):
 
     def transform_input(self, prompt: str, model_kwargs: Dict) -> bytes:
         self.len_prompt = len(prompt)
-        input_str = json.dumps({"inputs": prompt, "parameters": {"max_new_tokens": 50, "do_sample": False, "repetition_penalty": 1.1}})
+        input_str = json.dumps({"inputs": prompt, "max_new_tokens": 100, "do_sample": False, "repetition_penalty": 1.1})
         return input_str.encode('utf-8')
 
     def transform_output(self, output: bytes) -> str:
-        response_json = output.read()
-        res = json.loads(response_json)
-        # ans = res[0]['generated_text']
-        ans = res[0]['generated_text'][self.len_prompt:]
-        return ans
+        response = output.read()
+        res = json.loads(response)
+        # ans = res[0]['generated_text'][self.len_prompt:]
+        # ans = ans[:ans.rfind("Human:")]
+        return res
+
 
 
 content_handler = ContentHandler()
 llm = SagemakerEndpoint(
         endpoint_name=endpoint_name,
-        region_name="us-east-1",
+        region_name="eu-central-1",
         content_handler=content_handler,
-        credentials_profile_name="default"
+        #credentials_profile_name="default"
     )
 
 llm_chain = LLMChain(
